626ae6498fd316ff951690505cd05357ecc88803
Marcus Canul <94863812+MeMarcusC@users.noreply.github.com>
2022-11-22 17:07:37 -0600

    Create FinalProject.cpp

---
+++ FinalProject.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+/*
+The Pawn Champs Ltd. needs a program that will manage their transactions at the store. You were hired to develop such a 
+program using object oriented programming techniques using C++. A customer can come in and wish to pawn an item. 
+The item can be a piece of jewelry or an electronic item. You can consider using a class pawn that will contain all 
+the general characteristics (pawn number, balance, monthly payment etc) and methods that a regular pawn transaction 
+will possess. Methods can include paypayment, forfeitpawn etc. You can consider derived classes such as jewelrypawn 
+and electronicpawn. These classes will be derived from the pawn class.
+The program will implement this structure and allow for pawn transactions to be recorded. Also a customer can pay 
+monthly installments, forfeit a pawn and all the other methods you consider relevant or
+important. The program must use classes and objects. It must have a menu with all the options available. It must 
+print all messages or instructions in a professional and user friendly manner.
+A user manual must be written with screenshots to describe its functionality to its users.
+*/
+class account
+{
+
+};
+class Pawn
+{
+
+};
+class Jewelry: public Pawn
+{
+
+};
+class electronic: public Pawn
+{
+
+};
