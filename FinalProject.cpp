34fc78312affc421284acd611f954bc836cefac8
Marcus Canul <94863812+MeMarcusC@users.noreply.github.com>
2022-11-29 13:05:59 -0600

    okis

--- FinalProject.cpp
+++ FinalProject.cpp
@@ -15,7 +15,7 @@ important. The program must use classes and objects. It must have a menu with al
 print all messages or instructions in a professional and user friendly manner.
 A user manual must be written with screenshots to describe its functionality to its users.
 */
-class account
+class Account
 {
     private:
         string name;
@@ -24,16 +24,17 @@ class account
         int phone;
         bool record;
     public:
-        account();
-        void makepawn();
-
-//hi cluadia part 2
+        Account(string nm, int acc, string add, int ph, bool rec);
+        void makepawn(Account A );
+        int getid();
+        string gettype(int T);
 };
 class Pawn
 {
     private:
         int owner;
         int pawnid;
+        double worth;
         double interest;
         double balance;
         double monthlypayment;
@@ -63,3 +64,91 @@ class Electronic: public Pawn
     public:
         Electronic();
 };
+Account::Account(string nm, int acc, string add, int ph, bool rec)
+{
+    name= nm;
+    accnum=acc;
+    address=add;
+    phone= ph;
+    record= rec;  
+}
+
+int Account::getid()
+{
+    int id;
+    id = accnum;
+    return id;
+}
+
+string Account::gettype(int T)
+{
+    string type;
+    if (T == 1)
+    {
+        type="Mobile Phone";
+    }
+    else if (T == 2)
+    {
+        type="Computer";
+    }
+    else if (T == 3)
+    {
+        type="Television";
+    }
+    else if (T == 4)
+    {
+        type="Kitchen Appliance";
+    }
+    else if (T == 5)
+    {
+        type="Security Appliance";
+    }
+    else if (T == 6)
+    {
+        type="Sound & Light";
+    }
+    else if (T == 7)
+    {
+        type="Entertainment";
+    }
+    return type;
+}
+
+void Account::makepawn(Account A)
+{
+    int ans;
+    int owner;
+    int pawnid;
+    double worth;
+    double interest;
+    double balance;
+    double monthlypayment;
+    bool status;
+    owner = A.getid();
+    cout<<"\n\nPlease enter the Pawn Item ID: ";
+    cin>>pawnid;
+    cout<<"Please choose the type of item wished to be pawn (1 or 2).\n";
+    cout<<"\n1) Electronic Item\n2) Jewelry\n";
+    cout<<"Item: ";
+    cin>>ans;
+    if (ans == 1)
+    {
+        double timeusage;
+        string type;
+        int ty;
+        cout<<"Please enter the type of Electronic Item (1,2,3,4,5)...\n";
+        cout<<"\n1) Mobile Phone\n2) Computer\n3) Television\n4) Kitchen Appliance\n5) Security Appliance\n6) Network Tool\n7) Entertainment\n";
+        cout<<"Type: ";
+        cin>>ty;
+        type = A.gettype(ty);
+        cout<<"Please enter the Time Usage of the Item: ";
+        cin>>timeusage;
+
+    }
+    else if (ans == 2)
+    {
+
+    }
+    cout<<"Please enter the worth of the item: ";
+    cin>>worth;
+}
